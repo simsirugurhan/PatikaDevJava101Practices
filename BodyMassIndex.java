1ab25ec7188faaee1251f584b71e077fb192a982
Uğurhan Şimşir <87274613+simsirugurhan@users.noreply.github.com>
2022-10-30 02:17:33 +0300

    Update BodyMassIndex.java

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -6,17 +6,17 @@ public class BodyMassIndex {
 
 	public static void main(String[] args) {
 		//Kilo (kg) / Boy(m) * Boy(m)
-		double weight, height, index;
+		double weight, height, index; //Created variables
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in); // Created scanner for input from user
 		System.out.print("Kilo giriniz: ");
-		weight = scan.nextDouble(); 
+		weight = scan.nextDouble(); //scanning user's weight
 		
 		System.out.print("Boy giriniz (Metre Cinsinden): ");
-		height = scan.nextDouble();
+		height = scan.nextDouble(); //scanning user's height
 		
-		index = weight / (height * height);
-		System.out.println("Vücut kitle indeksiniz: " + index);
+		index = weight / (height * height); //calculating body mass index for user
+		System.out.println("VÃ¼cut kitle indeksiniz: " + index); //output of user's body mass index
 	}
 
 }
