73d25a041370c8ade57bed030556269d85dc1277
Uğurhan Şimşir <87274613+simsirugurhan@users.noreply.github.com>
2022-11-08 01:35:41 +0300

    Update CreateMethodFigure.java

--- CreateMethodFigure.java
+++ CreateMethodFigure.java
@@ -3,13 +3,13 @@ package averageNotes;
 import java.util.Scanner;
 
 public class CreateMethodFigure {
-	// Java dilinde kullanıcıdan alınan n değerine göre aşağıdaki kurala uyan döngü
-	// kullanmadan bir "Recursive" metot yazın.
+	// Java dilinde kullanÃ½cÃ½dan alÃ½nan n deÃ°erine gÃ¶re aÃ¾aÃ°Ã½daki kurala uyan dÃ¶ngÃ¼
+	// kullanmadan bir "Recursive" metot yazÃ½n.
 
-	// Kural : Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5
-	// rakamını çıkarın. Her çıkarma işlemi sırasında ekrana son değeri yazdırın.
-	// Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin. Yine her ekleme
-	// işleminde sayının son değerini ekrana yazdırın.
+	// Kural : Girilen sayÃ½ 0 veya negatif olduÃ°u yere kadar girilen sayÃ½dan 5
+	// rakamÃ½nÃ½ Ã§Ã½karÃ½n. Her Ã§Ã½karma iÃ¾lemi sÃ½rasÃ½nda ekrana son deÃ°eri yazdÃ½rÃ½n.
+	// SayÃ½ negatif veya 0 olduktan sonra tekrar 5 ekleyin. Yine her ekleme
+	// iÃ¾leminde sayÃ½nÃ½n son deÃ°erini ekrana yazdÃ½rÃ½n.
 
 	static void figure(int number, int original, int control) {
 		// controls
@@ -44,7 +44,6 @@ public class CreateMethodFigure {
 					figure(number - 5, original, 1);
 					return;
 				}
-				// if number is from negative to zero control will be 0
 			}
 		}
 	}
@@ -54,7 +53,7 @@ public class CreateMethodFigure {
 
 		Scanner scan = new Scanner(System.in); // scanner created
 
-		System.out.print("Sayı giriniz :"); // input number
+		System.out.print("SayÃ½ giriniz :"); // input number
 		number = scan.nextInt(); // number assigned to variable
 		
 		if (number < 0) {
